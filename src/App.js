e4e6d4ea518b40094a738ea5963f8520e9069bab
And111rey <samu111ray@gmail.com>
2019-05-05 23:56:54 +0300

    such as the second but purified

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
 import React, { Component } from 'react';
 import { WeatherNow } from "./components/weather-now/WeatherNow.js"
 
-// import axios from 'axios';
-
-// const name = "london";
-// const Api_Key = "00a0da177d20c57bd3a0bac393cea160";
-// const format = "units=metric";
-// const URL = `http://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${Api_Key}&${format}`;//Сборний УРЛ
-
-// const URL = `http://api.openweathermap.org/data/2.5/weather?q=london&appid=00a0da177d20c57bd3a0bac393cea160&units=metric`;//
-
 class App extends Component {
 
-  // state = {
-  //   data: {},
-  //   town: ""
-  // }
-//////..........with asis.....
-  // componentWillMount() {
-  //   this.getData();
-  // };
-  // getData() {
-  //   axios.get(URL)
-  //   .then(response => {
-  //     this.setState({data: response.data.main}, () => {
-  //       console.log(this.state.data);
-  //     })
-  //   })
-  // }
 
   state = {
     data: {},
@@ -52,38 +27,6 @@ class App extends Component {
       .catch(error => error)
   }
    
-    
-  
-  // setData = (result) => {
-  //   this.setState({
-  //     data:result.main
-  //   })
-  //   console.log(this.state.data);
-    
-  // }
-
-
-
-////////////////////////////////
-//////////////////////////////
-  // componentDidMount() {
-   
-  //   let URL = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.town}&appid=00a0da177d20c57bd3a0bac393cea160&units=metric`;//
-    
-  //   fetch(URL)
-  //     .then(res => res.json())
-  //     .then(result => this.setData(result))
-  //     .catch(error => error)
-  // }
-  // setData = (result) => {
-  //   this.setState({
-  //     data:result.main
-  //   })
-  //   console.log(this.state.data);
-    
-  // }
-
-  
   
   render () {
     const {data} = this.state;
@@ -98,15 +41,4 @@ class App extends Component {
   }
 }
 
-
-
-
-
-// const App = () => {
-//   return (
-//     <div>
-//       < WeatherNow />
-//     </div>
-//   );
-// }
 export { App };
